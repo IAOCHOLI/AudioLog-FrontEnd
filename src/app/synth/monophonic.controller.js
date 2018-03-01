e97691f98569490e0e50700d94a14f30bd98eac9
IAOCHOLI <32553785+IAOCHOLI@users.noreply.github.com>
2018-03-01 22:40:37 +0000

    added more synths

--- src/app/synth/monophonic.controller.js
+++ src/app/synth/monophonic.controller.js
@@ -10,3 +10,55 @@ var monophonicSynth = new Tone.Monophonic (
  monophonicSynth.setNote("F#6", "+4n");
 
 
+ //Insert Effect
+
+ var eq = new Tone.EQ3({
+    low: 0,
+    mid: 0,
+    high: 0,
+    lowFrequency: 400,
+    highFrequency: 2500
+  });
+ 
+var panner = new Tone.Panner({
+    Default: 0
+}
+);
+
+var filter = new Tone.Filter(
+    {
+        type  : lowpass ,
+        frequency  : 350 ,
+        rolloff  : -12 ,
+        Q  : 1 ,
+        gain  : 0
+    }
+);
+
+
+
+
+ //connections
+monophonicSynth.chain( panner, eq, filter, Tone.Master);
+
+
+
+
+// Appegio
+Tone.Transport.scheduleRepeat(function (time) {
+    var note = notes[current_note % notes.length];
+    synth.triggerAttackRelease(note, '16n', time);
+    current_note++;
+  }, '16n');
+
+
+
+
+  
+pad1.on('change' ,function(value){
+    if(value.state = true){
+    editPad(event, 'PadOneEdit');
+    vol.mute.value = true;
+    }
+  
+  })
